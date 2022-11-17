1fbd78a32d3bec2517390eac2eb268f5e5e69743
kubesqrt <96239298+kubesqrt@users.noreply.github.com>
2022-11-17 22:52:21 +0000

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+
+require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "print the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ether.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.REACT_APP_RINKEBY_RPC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    }
+
+  }
+};
